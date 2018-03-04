1583e33f5df2797a982bab50a72771d8dc2c6785
kalanda98 <nikala@ttu.ee>
2018-03-04 16:08:10 +0200

    5th week_ first task

---
+++ web-tech-5th-week/Conquer Free Website Template - Free-CSS.com/templatemo_476_conquer/js/change.js
@@ -0,0 +1,59 @@
+function mouseOver()
+{
+    document.getElementById("nika1").style.color = "black";
+    document.getElementById("nika1").style.backgroundColor = "white";
+
+}
+function mouseOut()
+{
+    document.getElementById("nika1").style.color = "white";
+    document.getElementById("nika1").style.backgroundColor = "#383E4C";
+}
+
+function mouseOver2()
+{
+    document.getElementById("nika2").style.color = "black";
+    document.getElementById("nika2").style.backgroundColor = "white";
+
+}
+function mouseOut2()
+{
+    document.getElementById("nika2").style.color = "white";
+    document.getElementById("nika2").style.backgroundColor = "#383E4C";
+}
+
+function mouseOver3()
+{
+    document.getElementById("nika3").style.color = "black";
+    document.getElementById("nika3").style.backgroundColor = "white";
+
+}
+function mouseOut3()
+{
+    document.getElementById("nika3").style.color = "white";
+    document.getElementById("nika3").style.backgroundColor = "#383E4C";
+}
+
+function mouseOver4()
+{
+    document.getElementById("nika4").style.color = "black";
+    document.getElementById("nika4").style.backgroundColor = "white";
+
+}
+function mouseOut4()
+{
+    document.getElementById("nika4").style.color = "white";
+    document.getElementById("nika4").style.backgroundColor = "#383E4C";
+}
+
+function mouseOver5()
+{
+    document.getElementById("nika5").style.color = "black";
+    document.getElementById("nika5").style.backgroundColor = "white";
+
+}
+function mouseOut5()
+{
+    document.getElementById("nika5").style.color = "white";
+    document.getElementById("nika5").style.backgroundColor = "#383E4C";
+}
